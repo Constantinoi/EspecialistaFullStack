7420e58ff243323f6701fdafdbf3cdbdd5dbc4fa
constantinoi <pescaalma@gmail.com>
2022-04-10 16:26:41 -0300

    Criando recursos da entidade categorias

--- src/main/java/com/algaworks/algamoneyapy/resource/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoneyapy/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.algaworks.algamoneyapy.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.algaworks.algamoneyapy.model.Categoria;
 import com.algaworks.algamoneyapy.repository.CategoriaRepository;
@@ -16,9 +25,25 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@GetMapping
-	public List<Categoria> listar(){
-		return categoriaRepository.findAll();
+	public ResponseEntity<?> listar() {
+		List<Categoria> lista = categoriaRepository.findAll();
+		return !lista.isEmpty() ? ResponseEntity.ok(lista) : ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Optional<Categoria> buscarPeloCodigo(@PathVariable Long codigo){
+		return categoriaRepository.findById(codigo);
 	}
 }

32bbf6d2f0f30e55ffd0cf58b4bea00dfcfa0fc1
constantinoi <pescaalma@gmail.com>
2022-04-10 16:03:31 -0300

    Configurando e criando primeira entidade

---
+++ src/main/java/com/algaworks/algamoneyapy/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.algaworks.algamoneyapy.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.algaworks.algamoneyapy.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}

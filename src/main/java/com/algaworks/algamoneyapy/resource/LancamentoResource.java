88bc679a9eb209b768c8407a6707ddef4fbeefc3
constantinoi <pescaalma@gmail.com>
2022-04-17 20:01:55 -0300

    validando-incosistencias

--- src/main/java/com/algaworks/algamoneyapy/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoneyapy/resource/LancamentoResource.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,7 +47,7 @@ public class LancamentoResource {
 
 	}
 
-	@PutMapping("/{codigo}")
+	@PostMapping
 	public ResponseEntity<?> salvar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
 
 		Lancamento lanc = lancamentoRepository.save(lancamento);

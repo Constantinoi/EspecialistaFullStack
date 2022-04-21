1afeea7f6b10ee1cfe06f4763e54ce6892f8f384
constantinoi <pescaalma@gmail.com>
2022-04-20 21:23:05 -0300

    criando Paginação

--- src/main/java/com/algaworks/algamoneyapy/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/algaworks/algamoneyapy/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,12 @@
 package com.algaworks.algamoneyapy.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.algaworks.algamoneyapy.model.Lancamento;
 import com.algaworks.algamoneyapy.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
- 
--- src/main/java/com/algaworks/algamoneyapy/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoneyapy/resource/LancamentoResource.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -48,10 +50,8 @@ public class LancamentoResource {
 	private MessageSource messageSource;
 
 	@GetMapping
-	public ResponseEntity<?> pesquisar(LancamentoFilter lancamentoFilter) {
-		List<Lancamento> lista = lancamentoRepository.findAll();
-
-		return !lista.isEmpty() ? ResponseEntity.ok(lista) : ResponseEntity.notFound().build();
+	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
 
 	@GetMapping("/{codigo}")
